2a60386a4d0f68b70733243055b5a72c134689b7
harmandhaliwal <harman-dhaliwal@live.ca>
2019-07-25 19:49:40 -0700

    Added appendPageLinks function

--- js/script.js
+++ js/script.js
@@ -49,14 +49,67 @@ const showPage = (list, page) => {
   }
    };
 
-
+//create element function
+const createElement = (element) => {
+   const tag = document.createElement(element);
+   return tag;
+};
 
 /*** 
    Create the `appendPageLinks function` to generate, append, and add 
    functionality to the pagination buttons.
 ***/
 
+const appendPageLinks = (list) => {
+   /*
+   1. Determine how many pages are needed for the list by dividing the
+   total number of list items by the max number of items per page
+   2. Create a div, give it the “pagination” class, and append it to the .page div
+   3. Add a ul to the “pagination” div to store the pagination links
+   4. for every page, add li and a tags with the page number text
+   5. Add an event listener to each a tag. When they are clicked
+   call the showPage function to display the appropriate page
+   6. Loop over pagination links to remove active class from all links
+   7. Add the active class to the link that was just clicked. You can identify that
+   clicked link using event.target
+   */
 
+  const pageDiv = createElement('div');
+  const pageUl = createElement('ul');
+  pageDiv.className = 'pagination';
+  document.querySelector(".page").appendChild(pageDiv);
+  pageDiv.appendChild(pageUl);
+
+  const pages = Math.ceil(list.length/itemsPerPage);
+  for (let i = 1; i <= pages; i++) {
+     const pageLi = createElement('li');
+     const pageA = createElement('a');
+     pageA.href = '#';
+     pageA.textContent = i;
+     pageUl.appendChild(pageLi);
+     pageLi.appendChild(pageA);
+     if (i === 1){
+      pageA.className = 'active';
+     }  
+  };
+
+  const a = document.querySelectorAll('.pagination ul li a');
+
+  for (let i = 0; i < a.length; i++){
+     a[i].addEventListener('click', (e) =>{
+        for (let j = 0; j < a.length; j++){
+           a[j].classList.remove('active');
+        };
+        e[i].target.classList.add('active');
+        showPage(list, e.target.textContent);
+      } )
+  };
+
+
+   };
+
+showPage(listOfStudents, 1);
+appendPageLinks(listOfStudents);
 
 
 
