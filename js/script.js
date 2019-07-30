00a1c39bc7d91bc9bb747a52e798c404a6d97926
harmandhaliwal <harman-dhaliwal@live.ca>
2019-07-29 17:26:51 -0700

    Add functionality to the search component and pagination of search results

--- js/script.js
+++ js/script.js
@@ -20,7 +20,6 @@ FSJS project 2 - List Filter and Pagination
 const listOfStudents = document.querySelectorAll('.student-item');
 const itemsPerPage = 10;
 
-
 /*** 
    Create the `showPage` function to hide all of the items in the 
    list except for the ten you want to show.
@@ -61,25 +60,16 @@ const createElement = (element) => {
 ***/
 
 const appendPageLinks = (list) => {
-   /*
-   1. Determine how many pages are needed for the list by dividing the
-   total number of list items by the max number of items per page
-   2. Create a div, give it the “pagination” class, and append it to the .page div
-   3. Add a ul to the “pagination” div to store the pagination links
-   4. for every page, add li and a tags with the page number text
-   5. Add an event listener to each a tag. When they are clicked
-   call the showPage function to display the appropriate page
-   6. Loop over pagination links to remove active class from all links
-   7. Add the active class to the link that was just clicked. You can identify that
-   clicked link using event.target
-   */
-
+   const old = document.querySelector('.pagination');
+   if (old != null) {
+      document.querySelector('.page').removeChild(old);
+   };
   const pageDiv = createElement('div');
   const pageUl = createElement('ul');
   pageDiv.className = 'pagination';
   document.querySelector(".page").appendChild(pageDiv);
   pageDiv.appendChild(pageUl);
-
+  
   const pages = Math.ceil(list.length/itemsPerPage);
   for (let i = 1; i <= pages; i++) {
      const pageLi = createElement('li');
@@ -94,7 +84,6 @@ const appendPageLinks = (list) => {
   };
 
   const a = document.querySelectorAll('.pagination ul li a');
-
   for (let i = 0; i < a.length; i++){
      a[i].addEventListener('click', (e) =>{
         for (let j = 0; j < a.length; j++){
@@ -104,9 +93,8 @@ const appendPageLinks = (list) => {
         showPage(list, e.target.textContent);
       } )
   };
-
-
    };
+
 
    const studentSearch = () => {
       const headerDiv = document.querySelector('.page-header');
@@ -120,7 +108,32 @@ const appendPageLinks = (list) => {
       searchDiv.appendChild(searchField);
       searchDiv.appendChild(searchButton);
 
+   searchButton.addEventListener('click', (e) => {
+      e.preventDefault();
+      const userInput = searchField.value;
+      const searchResults = userSearch(userInput, listOfStudents);
+      showPage(searchResults, 1);
+      appendPageLinks(searchResults);
+   });
+};
+
+
+const userSearch = (input, list) => {
+   let resultArray = []; 
+   if (!input) { 
+      return list;
+   } else {
+      for (let i = 0; i < list.length; i++) { 
+         list[i].style.display = 'none';
+         let studentName = list[i].querySelector('h3').textContent.toLowerCase();
+         if (studentName.includes(input.toLowerCase())){
+               resultArray.push(list[i]);
+         }
+      };
+      return resultArray;
    };
+};
+
 
 studentSearch();
 showPage(listOfStudents, 1);
